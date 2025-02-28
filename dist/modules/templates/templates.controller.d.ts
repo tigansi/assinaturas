5e76f35b9386df01ac732fda0c17f44176fa9a90
Tiago Igansi <tiagoigansi17@gmail.com>
2025-02-28 07:29:13 -0300

    add motor express

--- dist/modules/templates/templates.controller.d.ts
+++ dist/modules/templates/templates.controller.d.ts
@@ -1,5 +1,6 @@
-import { TemplatesService } from './templates.service';
+import { TemplatesService } from "./templates.service";
 export declare class TemplatesController {
     private readonly templatesService;
     constructor(templatesService: TemplatesService);
+    uploadDocx(): Promise<void>;
 }
--- dist/templates/templates.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import { TemplatesService } from './templates.service';
-export declare class TemplatesController {
-    private readonly templatesService;
-    constructor(templatesService: TemplatesService);
-}
